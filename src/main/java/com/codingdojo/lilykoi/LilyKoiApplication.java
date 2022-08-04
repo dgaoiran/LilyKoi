afcc822dba0e462f61f5ae714e6dff735480f778
dgaoiran <100329962+dgaoiran@users.noreply.github.com>
2022-08-03 17:21:27 -0700

    add a descriptive

---
+++ src/main/java/com/codingdojo/lilykoi/LilyKoiApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.lilykoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilyKoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilyKoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/lilykoi/LilyKoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.lilykoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilyKoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
